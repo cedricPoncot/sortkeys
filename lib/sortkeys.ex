5ad7299d4a282beca845de2bf87fbd284cac86b1
0x9dd <cedric.l.poncot@gmail.com>
2025-01-05 22:53:34 +0100

    Init package

---
+++ lib/sortkeys.ex
@@ -0,0 +1,29 @@
+defmodule Sortkeys do
+  @moduledoc """
+  Documentation for `Sortkeys`.
+  """
+
+  @doc """
+
+  Recursively sorts the keys of a map.
+
+  ## Exemples
+
+      iex> Sortkeys.sort(%{b: 0, a: %{b: %{b: 0, a: 0}, a: 1}, c: 0})
+      %{a: %{a: 1, b: %{a: 0, b: 0}}, b: 0, c: 0}
+
+  """
+  @spec sort(map()) :: any()
+  def sort(map) when is_map(map) do
+    map
+    |> Enum.map(fn {key, value} ->
+      if is_map(value) do
+        {key, sort(value)}
+      else
+        {key, value}
+      end
+    end)
+    |> Enum.sort_by(fn {key, _value} -> key end)
+    |> Enum.into(%{})
+  end
+end
---
+++ test/sortkeys_test.exs
@@ -0,0 +1,24 @@
+defmodule SortkeysTest do
+  use ExUnit.Case
+  doctest Sortkeys
+
+  test "simple key sort" do
+    data = %{a: 0, c: 0, b: 0}
+    schema = %{a: 0, b: 0, c: 0}
+
+    assert Sortkeys.sort(data) == schema
+  end
+
+  test "deep key sort" do
+    data = %{
+      b: 0,
+      a: %{b: %{b: 0, a: 0}, a: 1},
+      c: 0
+    }
+    schema = %{
+      a: %{a: 1, b: %{a: 0, b: 0}},
+      b: 0,
+      c: 0
+    }
+  end
+end
